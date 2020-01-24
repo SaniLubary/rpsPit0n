fde62648449a39536c94db9ca3c00dbb992cfc9c
Santiago Lubary <santiago.lp.cop@gmail.com>
2020-01-24 14:52:36 -0300

    holy shit puedo commitear asi nomas?

--- main.py
+++ main.py
@@ -3,15 +3,14 @@
 import time
 from random import randint
 import sys
-from termcolor import colored, cprint
 
 # function to know the name each number refers to
 def strNameRefInt(i):
-	print("name ref, val: " + str(i))
+	# print("name ref, val: " + str(i))
 	switcher = {
 		1: "Rock",
 		2: "Paper",
-		3: "Scissors"
+		3: 'Scissors'
 	}
 	name = switcher.get(i, "Invalid number")
 	return name
@@ -41,33 +40,35 @@ def wait():
 		x += 1
 
 def winner(iPlayer, iPc):
-	result = "No result"
+	# 2 is draw, 0 is you and 1 is pc 
+	# [becouse in the array, the first value is What choice you kill, and the second What choice kills you]
+	result = 2
 
-	#define possibilities
-	#index 0 is the number you kill, index 1 is you die by that number
-	one = [3, 2];
-	two = [1, 3];
-	three = [2, 1];
-
-	#to define wich posibility is for the player
-	#this way you select the array that corresponds to the player's selection
-	switcher = {
-		1: one,
-		2: two,
-		3: three
-	}
-	arr = switcher.get(iPlayer, "error:What paso? O.o")
-
-	#set winner
+	# set winner
 	if iPc == iPlayer:
-		result = 3
+		pass
 	else:	
+		# define possibilities
+		# index 0 is the number you kill, index 1 is you die by that number
+		one = [3, 2];
+		two = [1, 3];
+		three = [2, 1];
+		
+		# to define wich posibility is for the player
+		# this way you select the array that corresponds to the player's selection
+		switcher = {
+			1: one,
+			2: two,
+			3: three
+		}
+		
+		arr = switcher.get(iPlayer, "error:What paso? O.o")
+
+		# with that array check if pc's choice kills you or gets killed by you
 		for i, v in enumerate(arr):
 			if v == iPc:
 				result = i
-			else:
-				#if passed twice, result , so draw!
-				pass
+				return result
 
 	return result
 	
@@ -86,12 +87,19 @@ def play():
 
 	iWinner = winner(iPlayerChoice, iPcChoice)
 
-	# set choices to respective names of choices
-	sWinner			= strNameRefInt(iWinner)
 	sPlayerChoice 	= strNameRefInt(iPlayerChoice)
 	sPcChoice 		= strNameRefInt(iPcChoice)
 	print( sPlayerChoice + " vs " + sPcChoice + "!!")
-	print( sWinner + " wins!" )
+
+	# set choices to respective names of choices
+	if iWinner < 2:
+		if iWinner == 0:
+			winMessage = "\033[0;31mYou Win!\033[0m"
+		else:
+			winMessage = "PC Wins!"
+		print( winMessage )
+	else:
+		print( " Draw! ")
 
 
 
